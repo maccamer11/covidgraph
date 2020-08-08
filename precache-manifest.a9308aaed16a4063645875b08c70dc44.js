0c29245cfa1297198b1795f8040eb0396f10f702
Das <maccameron@live.ca>
2020-08-08 16:54:29 -0600

    Updates

---
+++ precache-manifest.a9308aaed16a4063645875b08c70dc44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05e6c58026fefdb9d090848fcc7f2d42",
+    "url": "/covidgraph/index.html"
+  },
+  {
+    "revision": "784f155dc9c3397975d0",
+    "url": "/covidgraph/static/css/main.d52fc236.chunk.css"
+  },
+  {
+    "revision": "6065e68abfea46373e99",
+    "url": "/covidgraph/static/js/2.25bb455e.chunk.js"
+  },
+  {
+    "revision": "2c24acf63e18487b21e343b8335b2718",
+    "url": "/covidgraph/static/js/2.25bb455e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "784f155dc9c3397975d0",
+    "url": "/covidgraph/static/js/main.ef95f983.chunk.js"
+  },
+  {
+    "revision": "75e24474932650d49dff",
+    "url": "/covidgraph/static/js/runtime-main.70c568bf.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covidgraph/static/media/covid.d7265326.png"
+  }
+]);
